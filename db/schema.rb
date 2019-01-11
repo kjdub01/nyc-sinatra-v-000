e3159b0f79166f534cc2db49601859fa4d40665e
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-01-11 00:41:11 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190107184921) do
-
-  create_table "figure_titles", force: :cascade do |t|
-    t.integer "title_id"
-    t.integer "figure_id"
-  end
+ActiveRecord::Schema.define(version: 20190107184910) do
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
